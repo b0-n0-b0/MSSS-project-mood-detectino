b39bbdf70480c6ce462cb4ee76dc63e94c5beac1
b0n0b0 <geraci2e@gmail.com>
2024-05-07 14:44:06 +0200

    feat: added Spotify app presence check

--- client/MSSSfeel_your_music/app/src/main/java/com/example/msss_feel_your_music/MainActivity.kt
+++ client/MSSSfeel_your_music/app/src/main/java/com/example/msss_feel_your_music/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.msss_feel_your_music
 
+import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.material3.Text
 import com.spotify.android.appremote.api.ConnectionParams
 import com.spotify.android.appremote.api.Connector
 import com.spotify.android.appremote.api.SpotifyAppRemote
+import com.spotify.android.appremote.api.error.CouldNotFindSpotifyApp
+import com.spotify.android.appremote.api.error.NotLoggedInException
+import com.spotify.android.appremote.api.error.UserNotAuthorizedException
 import com.spotify.protocol.types.Track
 
 
@@ -24,13 +31,12 @@ class MainActivity : ComponentActivity() {
         super.onStart()
         val clientId = getString(R.string.CLIENT_ID)
         val redirectUri = getString(R.string.REDIRECT_URI)
-
         val connectionParams = ConnectionParams.Builder(clientId)
             .setRedirectUri(redirectUri)
             .showAuthView(true)
             .build()
-
-        SpotifyAppRemote.disconnect(spotifyAppRemote);
+        //NOTE: tries to connect to the spotify application
+//        SpotifyAppRemote.disconnect(spotifyAppRemote);
         SpotifyAppRemote.connect(this, connectionParams, object : Connector.ConnectionListener {
             override fun onConnected(appRemote: SpotifyAppRemote) {
                 spotifyAppRemote = appRemote
@@ -38,7 +44,20 @@ class MainActivity : ComponentActivity() {
             }
             override fun onFailure(error: Throwable) {
                 Log.d("Main Activity","Failed to connect")
-                showSpotifyLogin()
+                when (error) {
+                    is CouldNotFindSpotifyApp -> {
+                        if (!SpotifyAppRemote.isSpotifyInstalled(applicationContext)){
+                            val builder = AlertDialog.Builder(this@MainActivity)
+                            builder.setMessage("This app needs Spotify installed on the device!")
+                                .setCancelable(false)
+                                .setPositiveButton("Got it!") { dialog, _ ->
+                                    finish()
+                                }
+                            val alert = builder.create()
+                            alert.show()
+                        }
+                    }
+                }
             }
         })
     }
@@ -46,16 +65,23 @@ class MainActivity : ComponentActivity() {
     private val spotifyLoginLauncher = registerForActivityResult<Intent, ActivityResult>(
         ActivityResultContracts.StartActivityForResult()
     ) { result: ActivityResult? ->
-        //TODO: add result handling
-    }
-
-    private fun showSpotifyLogin(){
-        setContentView(R.layout.authorize_app)
-        val button = findViewById<Button>(R.id.connectSpotifyButton)
-        button.setOnClickListener {
-            val intent = Intent(this, SpotifyLoginActivity::class.java)
-            spotifyLoginLauncher.launch(intent)
-        }
+            result?.let { activityResult ->
+                if (activityResult.resultCode == Activity.RESULT_OK) {
+                    val data: Intent? = activityResult.data
+                    data?.let {
+                        val extras = it.extras
+                        extras?.let { bundle ->
+                            val result = bundle.getBoolean("success")
+                            Log.d("something","Success: $result")
+                            if(result){
+                                connected()
+                            }
+                        }
+                    }
+                } else {
+                    //TODO: handle error
+                }
+            }
     }
 
     private fun connected() {
